63ade9bc1dd2aeb4d3f0a2539713ed165c309b3e
Nick Petzold <nickpetzold@gmail.com>
2018-12-03 15:04:01 +0000

    Devise setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Inlcuded after prompt during installation
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
