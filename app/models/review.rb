1d5aa31469bef572af9fb7daef3b848d2bd70da1
Nick Petzold <nickpetzold@gmail.com>
2018-12-03 18:04:32 +0000

    Review requires rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :booking
   validates :booking_id, presence: true, uniqueness: true
   validates :content, length: { minimum: 50, maximum: 500 }
-  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+  validates :rating, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
