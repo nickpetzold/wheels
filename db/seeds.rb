2bab61642914dd01387e9b7986090a7bca8ac26d
filipe custodio <filipe.l.custodio@gmail.com>
2018-12-05 13:25:14 +0000

    fixed addresses so we don't have cars in the philiphines instead of lisbon lmao

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,70 +22,70 @@ end
 puts "Creating Cars"
 car = Car.new(brand: "BMW", model: "i8", year: 2018, passengers: 4, color: "white", horsepower: 400, price_per_day: 150, fuel_type: "electric", city: "Lisbon", zipcode: "1000-028", country: "Portugal")
 car.user = User.last
-car.address = "#{car["city"]},#{car["zipcode"]}"
+car.address = "Avenida de Berna 45,#{car["city"]}"
 car.photo ="https://res.cloudinary.com/dajppgk4c/image/upload/v1544011669/cars%20-%20wheels/567830182340f87d008b72f3-750-563.jpg"
 car.car_type = CarType.order("RANDOM()").first
 car.save
 
 car = Car.new(brand: "Mercedes", model: "C", year: 2017, passengers: 4, color: "black", horsepower: 300, price_per_day: 85, fuel_type: "diesel", city: "Lisbon", zipcode: "1000-058", country: "Portugal")
 car.user = User.last
-car.address = "#{car["city"]},#{car["zipcode"]}"
+car.address = "Rua das trinas,#{car["city"]}"
 car.photo = "https://res.cloudinary.com/dajppgk4c/image/upload/v1544012611/cars%20-%20wheels/photo-mercedesC.jpg"
 car.car_type = CarType.order("RANDOM()").first
 car.save
 
 car = Car.new(brand: "Porsche", model: "Cayenne", year: 2016, passengers: 6, color: "brown", horsepower: 400, price_per_day: 140, fuel_type: "petrol", city: "Lisbon", zipcode: "1000-070", country: "Portugal")
 car.user = User.last
-car.address = "#{car["city"]},#{car["zipcode"]}"
+car.address = "Rua Professor Lima Basto,#{car["city"]}"
 car.photo ="https://res.cloudinary.com/dajppgk4c/image/upload/v1544012927/cars%20-%20wheels/2015-Porsche-Cayenne.jpg"
 car.car_type = CarType.order("RANDOM()").first
 car.save
 
 car = Car.new(brand: "Lamborghini", model: "Aventador", year: 2016, passengers: 2, color: "orange", horsepower: 750, price_per_day: 200, fuel_type: "gas", city: "Lisbon", zipcode: "1000-101", country: "Portugal")
 car.user = User.last
-car.address = "#{car["city"]},#{car["zipcode"]}"
+car.address = "Rua Padre António Vieira 1,#{car["city"]}"
 car.photo = "https://res.cloudinary.com/dajppgk4c/image/upload/v1544012968/cars%20-%20wheels/lambo-orange.jpg"
 car.car_type = CarType.order("RANDOM()").first
 car.save
 
 car = Car.new(brand: "Audi", model: "R8", year: 2012, passengers: 2, color: "silver", horsepower: 580, price_per_day: 150, fuel_type: "petrol", city: "Lisbon", zipcode: "1000-102", country: "Portugal")
 car.user = User.last
-car.address = "#{car["city"]},#{car["zipcode"]}"
+car.address = "Torre de Belem,#{car["city"]}"
 car.photo = "https://res.cloudinary.com/dajppgk4c/image/upload/v1544013059/cars%20-%20wheels/silver-audi-r8.jpg"
 car.car_type = CarType.order("RANDOM()").first
 car.save
 
 car = Car.new(brand: "VW", model: "Golf", year: 2009, passengers: 4, color: "red", horsepower: 200, price_per_day: 35, fuel_type: "diesel", city: "Lisbon", zipcode: "1000-140", country: "Portugal")
 car.user = User.last
-car.address = "#{car["city"]},#{car["zipcode"]}"
+car.address = "Avenida 5 de Outubro,#{car["city"]}"
 car.photo = "https://res.cloudinary.com/dajppgk4c/image/upload/v1544013119/cars%20-%20wheels/vw-golf-red.jpg"
 car.car_type = CarType.order("RANDOM()").first
 car.save
 
 car = Car.new(brand: "Mini", model: "Cooper JCW", year: 2015, passengers: 4, color: "green", horsepower: 210, price_per_day: 60, fuel_type: "petrol", city: "Lisbon", zipcode: "1000-143", country: "Portugal")
 car.user = User.last
-car.address = "#{car["city"]},#{car["zipcode"]}"
+car.address = "Avenida Fontes Pereira de Melo,#{car["city"]}"
 car.photo = "https://res.cloudinary.com/dajppgk4c/image/upload/v1544013183/cars%20-%20wheels/mini-jsw-green.jpg"
 car.car_type = CarType.order("RANDOM()").first
 car.save
 
 car = Car.new(brand: "Ferrari", model: "F12 Berlinetta", year: 2016, passengers: 2, color: "red", horsepower: 900, price_per_day: 300, fuel_type: "gas", city: "Lisbon", zipcode: "1000-188", country: "Portugal")
 car.user = User.last
-car.address = "#{car["city"]},#{car["zipcode"]}"
+car.address = "Rua Carlos Testa,#{car["city"]}"
 car.photo = "https://res.cloudinary.com/dajppgk4c/image/upload/v1544013217/cars%20-%20wheels/ferrari-berli-red.jpg"
 car.car_type = CarType.order("RANDOM()").first
 car.save
 
 car = Car.new(brand: "Aston Martin", model: "DB12", year: 2010, passengers: 4, color: "black", horsepower: 750, price_per_day: 280, fuel_type: "petrol", city: "Lisbon", zipcode: "1000-190", country: "Portugal")
 car.user = User.last
-car.address = "#{car["city"]},#{car["zipcode"]}"
+car.address = "Santa apolonia,#{car["city"]}"
 car.photo = "https://res.cloudinary.com/dajppgk4c/image/upload/v1544013265/cars%20-%20wheels/astonmartin_db12_black.jpg"
 car.car_type = CarType.order("RANDOM()").first
 car.save
 
 car = Car.new(brand: "Jaguar", model: "F-Type", year: 2017, passengers: 2, color: "black", horsepower: 650, price_per_day: 220, fuel_type: "diesel", city: "Lisbon", zipcode: "1000-195", country: "Portugal")
 car.user = User.last
-car.address = "#{car["city"]},#{car["zipcode"]}"
+car.address = "Alta de Lisboa,#{car["city"]}"
 car.photo = "https://res.cloudinary.com/dajppgk4c/image/upload/v1544013312/cars%20-%20wheels/jaguar-ftype-black.jpg"
 car.car_type = CarType.order("RANDOM()").first
 car.save
