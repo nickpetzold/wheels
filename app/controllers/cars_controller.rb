028de605c6b161d6ff34d75d3ac892a722031cf9
Ruben Dias <rubenmsd@hotmail.com>
2018-12-05 01:58:16 +0000

    created cars new, and the form, made some style changes on the form to pick the dates, and a bunch of little tweaks

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -9,4 +9,28 @@ class CarsController < ApplicationController
     @reviews = @car.reviews
     @booking = Booking.new
   end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save # results in true or false
+      redirect_to @car
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :year, :passengers, :price_per_day, :address, :fuel_type, :city, :country, :car_type_id, :zipcode)
+  end
+
 end
