0806ed276b12097d4cdbeccf5ef0cf5d7151a9c1
filipe custodio <filipe.l.custodio@gmail.com>
2018-12-06 11:57:52 +0000

    added review/new html plus the controller method new and review params in private

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :rating)
+  end
+
 end
+
