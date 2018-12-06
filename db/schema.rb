3e17f8e99ff6e1cb4f7a48833707b659e9c7dbec
Nick Petzold <nickpetzold@gmail.com>
2018-12-06 15:01:06 +0000

    Seeds file complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_113424) do
+ActiveRecord::Schema.define(version: 2018_12_06_123303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
