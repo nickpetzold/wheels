d9f4a226d806bd6da54982c60f565760947ee889
Ruben Dias <rubenmsd@hotmail.com>
2018-12-06 15:44:00 +0000

    deleted disabled dates on datepicker,changed the car and bookings list from table to cards added total profit to the dashboard, added profit per car on dashboard, and styled dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -26,13 +26,24 @@ class BookingsController < ApplicationController
   def update
     @booking = Booking.find(params[:id])
     @booking.update(booking_params)
-    redirect_to cars_path
+    redirect_to dashboard_path(current_user)
   end
 
   def dashboard
     @bookings = Booking.where(user_id: current_user)
     @reviews = Review.where(booking_id: @bookings)
     @cars = Car.where(user_id: current_user)
+    # This is a function to get the total profit
+    @total_profit = 0
+    @bookings.each do |booking|
+      @total_profit = @total_profit + booking.price
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to cocktails_path
   end
 
   private
