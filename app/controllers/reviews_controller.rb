15e4f9d637853b211125f5c36b67f7febdaf3bbe
filipe custodio <filipe.l.custodio@gmail.com>
2018-12-06 12:46:56 +0000

    added edit functionality to reviews. Created the edit html. After the user creates or edits a specific review he will be redirected to dashboard

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,14 +9,18 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.booking = Booking.find(params[:booking_id])
     @review.save
+    redirect_to dashboard_path
   end
 
   def edit
-
+    @booking = Booking.find(params[:id])
+    @review = Review.find(params[:id])
   end
 
   def update
-
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to dashboard_path
   end
 
   private
